b17dfc067af448d50d24d608fa5f70bc73475d36
Pelado1998 <rafaelrodriguezsanz@gmail.com>
2020-09-15 20:32:30 -0300

    Agregados metodos a character

--- src/Library/Character.cs
+++ src/Library/Character.cs
@@ -1,12 +1,48 @@
 using System;
+using System.Collections.Generic;
 
 namespace Program
 {
     class Character
     {
-        private String name{get;}
-        private object type {get;}
-        private int life {get;}
-        private object inventory {get;}   
+        private String Name {get;}
+        private TypeCharacter Type {get;}
+        private int Life {get;set;}
+        private List<Item> Inventory {get;}   
+        public Character(String name,String type, List<Item> inventory)
+        {
+            this.Name = name;
+            this.Type = TypeCharacter.StatSelector(type);
+            this.Life = Type.MaxLife;
+            this.Inventory = inventory;
+        }
+        
+        public void Heal ()
+        {
+            this.Life = this.Type.MaxLife;
+        }
+        public void RecieveAttack(int attack)
+        {
+            this.Life = this.Life - attack;
+        }
+        public int AttackCalculator()
+        {
+            int res=this.Type.Attack;
+            foreach (Item item in this.Inventory)
+            {
+                res+=(item.Attack+item.Magic)*item.Force;
+            }
+            return res;
+        }
+        public int DefenceCalculator()
+        {
+            int res=this.Type.Defense;
+            foreach (Item item in this.Inventory)
+            {
+                res+=item.Defense+item.Resistence;
+            }
+            return res;
+        }
+
     }
 }
