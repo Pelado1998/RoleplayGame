d74b97dd9c7fb1c7557123329776894eb0ff13ee
Pelado1998 <rafaelrodriguezsanz@gmail.com>
2020-09-15 19:27:20 -0300

    Agregado clase Character

---
+++ src/Library/Character.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Program
+{
+    class Character
+    {
+        private String name{get;}
+        private object type {get;}
+        private int life {get;}
+        private object inventory {get;}   
+    }
+}
