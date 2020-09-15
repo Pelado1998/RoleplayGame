cfe23d2dd42b766ada00ba728c9994191faa2717
EstefanoGiusiano <estefanogiusiano2607@gmail.com>
2020-09-15 20:06:02 -0300

    agregado stacks a Item

--- src/Library/Item.cs
+++ src/Library/Item.cs
@@ -13,13 +13,20 @@ namespace Program
         public String Type { get; set; }
         public int Attack { get; set; }
         public int Defense { get; set; }
+        public int Force { get; set; }
+        public int Resistence { get; set;}
 
-        public Item(String name, String type, int attack, int defense)
+        public int Magic { get; set; }
+
+        public Item(String name, String type, int attack, int defense,int force,int resistence,int magic)
         {
             this.Name = name;
             this.Type = type;
             this.Attack = attack;
             this.Defense = defense;
+            this.Force=force;
+            this.Resistence=resistence;
+            this.Magic=Magic;
         }
     }
 }
