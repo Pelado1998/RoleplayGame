c73a2754911d7e701446d8d2e8a42fd5480489d0
CristianVelazquez1995 <58026968+CristianVelazquez1995@users.noreply.github.com>
2020-09-17 13:00:31 -0300

    Cambios varios

--- src/Library/Item.cs
+++ src/Library/Item.cs
@@ -5,35 +5,38 @@ namespace Program
     public class Item
     {
         public String Name { get; set; }
-        public TypeCharacter Type { get; set; }
+        public String CharacterType { get; set; }
+        public String Type { get; set; }
         public int Attack { get; set; }
         public int Defense { get; set; }
         public String Description { get; set; }
         public int Force { get; set; }
-        public int Resistence { get; set;}
+        public int Resistence { get; set; }
 
         public int Magic { get; set; }
 
-        public Item(String name, String type, int attack, int defense,int force,int resistence,int magic)
+        public Item(String name, String characterType, String type, int attack, int defense, int force, int resistence, int magic)
         {
             this.Name = name;
-            this.Type = new TypeCharacter(type);
+            this.CharacterType = characterType;
+            this.Type = type;
             this.Attack = attack;
             this.Defense = defense;
-            this.Force=force;
-            this.Resistence=resistence;
-            this.Magic=Magic;
+            this.Force = force;
+            this.Resistence = resistence;
+            this.Magic = Magic;
             this.Description = "Just an ordinary item";
         }
-        public Item(String name, String type, int attack, int defense,int force,int resistence,int magic, String description)
+        public Item(String name, String characterType, String type, int attack, int defense, int force, int resistence, int magic, String description)
         {
             this.Name = name;
-            this.Type = new TypeCharacter(type);
+            this.CharacterType = characterType;
+            this.Type = type;
             this.Attack = attack;
             this.Defense = defense;
-            this.Force=force;
-            this.Resistence=resistence;
-            this.Magic=Magic;
+            this.Force = force;
+            this.Resistence = resistence;
+            this.Magic = Magic;
             this.Description = description;
         }
     }
